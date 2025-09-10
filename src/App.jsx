33c64f3942db1bf6ce8f7fc558b00134ef276a29
git-Sjd <sajid.tisl@gmail.com>
2025-09-10 20:30:30 +0530

    storing the previous questions and their output

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,102 @@
-import { useState } from 'react'
-import './App.css'
-import { URL } from './utils/constant';
-import Answers from './components/Answers';
+import { useState } from "react";
+import "./App.css";
+import { URL } from "./utils/constant";
+import Answers from "./components/Answers";
 
 function App() {
-
-  const [question, setQuestion] = useState('');
-  const [result, setResult] = useState(undefined);
+  const [question, setQuestion] = useState("");
+  const [result, setResult] = useState([]);
 
   const payload = {
-    "contents": [
+    contents: [
       {
-        "parts": [
+        parts: [
           {
-            "text": question
-          }
-        ]
-      }
-    ]
-  }
+            text: question,
+          },
+        ],
+      },
+    ],
+  };
 
   const askQuestion = async () => {
-    let response = await fetch(URL + "AIzaSyAthYlH92j0PiGRan4Fwd4MbXY6Aw9SKkg", {
-      method: "POST",
-      body: JSON.stringify(payload)
-    });
+    let response = await fetch(
+      URL + "AIzaSyAthYlH92j0PiGRan4Fwd4MbXY6Aw9SKkg",
+      {
+        method: "POST",
+        body: JSON.stringify(payload),
+      }
+    );
 
     response = await response.json();
     let rawData = response.candidates[0].content.parts[0].text;
     rawData = rawData.split("* ");
     let refinedData = rawData.map((item, index) => item.trim());
     // console.log("refinedData: -->", refinedData);
-    setResult(refinedData)
-  }
+    setResult([
+      ...result,
+      { type: "qsn", qsnText: question },
+      { type: "ans", ansText: refinedData },
+    ]);
+  };
+
+  console.log("-->", result);
 
   return (
-    <div className='grid grid-cols-5 h-screen text-center'>
+    <div className="grid grid-cols-5 h-screen text-center">
+      <div className="col-span-1 bg-zinc-800"></div>
 
-      <div className='col-span-1 bg-zinc-800'>
-      </div>
-
-      <div className='col-span-4 p-10'>
-        <div className='container h-110 overflow-scroll'>
-          <div className='text-white '>
-            {/* {result} */}
+      <div className="col-span-4 p-10">
+        <div className="container h-110 overflow-scroll">
+          <div className="text-white ">
             <ul>
-              {
-                result && result.map((item, index) =>
-                  <li className='text-left ml-2 mt-2'>
-                    <Answers ans={item} index={index} />
-                  </li>)
-              }
+              {result.map((item, index) =>
+                item.type == "qsn" ? (
+                  <li key={index} className="text-left ml-2 mt-2 font-semibold">
+                    <Answers
+                      ans={item.qsnText}
+                      index={index}
+                      ansLength={item.length}
+                    />
+                  </li>
+                ) : (
+                  <li key={index} className="text-left ml-2 mt-2 pb-5 text-neutral-200">
+                    <Answers
+                      ans={item.ansText}
+                      index={index}
+                      ansLength={item.length}
+                    />
+                  </li>
+                )
+              )}
             </ul>
+
+            {/* <ul>
+              {result &&
+                result.map((item, index) => (
+                  <li key={index} className="text-left ml-2 mt-2">
+                    <Answers ans={item} index={index} ansLength={item.length} />
+                  </li>
+                ))}
+            </ul> */}
           </div>
         </div>
 
-        <div className='flex bg-zinc-800 w-1/2 mt-5 m-auto text-white rounded-4xl p-1 pr-5 border border-zinc-700 h-16'>
-          <input type='text' value={question} onChange={(event) => setQuestion(event.target.value)} className='w-full h-full outline-none p-3' placeholder='Ask me Anything' />
-          <button className='cursor-auto' onClick={askQuestion}>Ask</button>
+        <div className="flex bg-zinc-800 w-1/2 mt-5 m-auto text-white rounded-4xl p-1 pr-5 border border-zinc-700 h-16">
+          <input
+            type="text"
+            value={question}
+            onChange={(event) => setQuestion(event.target.value)}
+            className="w-full h-full outline-none p-3"
+            placeholder="Ask me Anything"
+          />
+          <button className="cursor-pointer" onClick={askQuestion}>
+            Ask
+          </button>
         </div>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -1,25 +1,30 @@
 import { useEffect, useState } from "react";
 import { checkHeading, headingStart } from "../utils/helper";
 
-const Answers = ({ ans, index }) => {
+const Answers = ({ ans, ansLength, index }) => {
     const [heading, setHeading] = useState(false);
-    const [answer, setAnswer] = useState(ans)
+    const [answer, setAnswer] = useState(ans);
+
+    // console.log("---->", ansLength, ans, index, heading, answer);
 
     useEffect(() => {
-
         if (checkHeading(ans)) {
             setHeading(true);
-            setAnswer(headingStart(ans))
+            setAnswer(headingStart(ans));
         }
-
-    }, [])
-
+    }, []);
 
     return (
         <>
-            {heading ? <span className="text-3xl">{ans}</span> : <span>{ans}</span>}
+            {index == 0 && ansLength > 1 ? (
+                <span className="text-2xl pb-10 font-bold">{answer}</span>
+            ) : heading ? (
+                <span className="text-xl pb-2 ">{answer}</span>
+            ) : (
+                <span className="pl-3 inline-block">{ans}</span>
+            )}
         </>
-    )
-}
+    );
+};
 
 export default Answers;
