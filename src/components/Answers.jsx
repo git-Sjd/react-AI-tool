61e1379e85525ec0ef131640f9b63e77592e833c
git-Sjd <sajid.tisl@gmail.com>
2025-09-13 00:16:51 +0530

    code block for answers

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import { checkHeading, headingStart } from "../utils/helper";
+import ReactMarkdown from "react-markdown";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 const Answers = ({ ans, ansLength, index }) => {
   const [heading, setHeading] = useState(false);
@@ -14,6 +17,25 @@ const Answers = ({ ans, ansLength, index }) => {
     }
   }, []);
 
+  const codeRenderer = {
+    code({ node, inline, className, children, ...props }) {
+      const match = /language-(\w+)/.exec(className || "");
+      return !inline && match ? (
+        <SyntaxHighlighter
+          {...props}
+          children={String(children).replace(/\n$/, "")}
+          language={match[1]}
+          style={dark}
+          PreTag="div"
+        />
+      ) : (
+        <code {...props} className={className}>
+          {children}
+        </code>
+      );
+    },
+  };
+
   return (
     <>
       {index == 0 && ansLength > 1 ? (
@@ -21,7 +43,9 @@ const Answers = ({ ans, ansLength, index }) => {
       ) : heading ? (
         <span className="text-xl pb-2 ">{answer}</span>
       ) : (
-        <span className="px-3 inline-block">{ans}</span>
+        <span className="px-3 inline-block">
+          <ReactMarkdown components={codeRenderer}>{ans}</ReactMarkdown>
+        </span>
       )}
     </>
   );
